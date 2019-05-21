e64e4e3215f64276da782458a499e471dd256dca
Nigel <nigelhing1@gmail.com>
2019-05-22 00:22:46 +0800

    f

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,15 +18,4 @@ Myflix::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-
-  config.eager_load = false
-   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
 end
