c98f76734bb997b0aa9558ce623a756d20db75cf
Nigel <nigelhing1@gmail.com>
2019-05-27 19:13:57 +0800

    f

--- app/assets/javascripts/payment.js
+++ app/assets/javascripts/payment.js
@@ -1,6 +1,6 @@
 $(function() {
 	$('#payment-form').find('form').submit(function(event) {
-		let $form = $(this);
+		var $form = $(this);
 
 		$form.find('button').prop('disabled', true);
 		Stripe.createToken($form, stripeResponseHandler);
@@ -14,10 +14,8 @@ let stripeResponseHandler = function(status, response) {
   if (response.error) { // Problem!
     $form.find('.payment-errors').text(response.error.message);
     $form.find('button').prop('disabled', false); 
-    console.log('fail');// Re-enable submission
   } else { // Token was created!
     var token = response.id;
-    console.log(token);
 
     // Insert the token into the form so it gets submitted to the server:
     $form.append($('<input type="hidden" name="stripeToken" />').val(token));
